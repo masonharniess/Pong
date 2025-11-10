9690f83e10690518636eecd7aca93e6cd74cddb1
Mason Harniess <102977508+masonharniess@users.noreply.github.com>
2025-11-10 20:51:30 +0000

    feat: implement FPS logic & screen painting
    - Implements the Time class, with the getTime() method, used to measure how much time has passed since the program started.
    - Adds time logic to game loop for measuring frame time.
    - Limits the frame rate at approximately 30fps using Thread.sleep().
    - Paints the screen black every frame via update().

--- src/Window.java
+++ src/Window.java
@@ -1,6 +1,9 @@
 import javax.swing.JFrame;
+import java.awt.*;
 
 public class Window extends JFrame implements Runnable {
+
+    Graphics2D g2;
 
     public Window () {
         this.setSize(Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
@@ -8,11 +11,24 @@ public class Window extends JFrame implements Runnable {
         this.setResizable(false);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        g2 = (Graphics2D)this.getGraphics();
+    }
+
+    public void update(double dt) {
+        g2.setColor(Color.BLACK);
+        g2.fillRect(0, 0, Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
     }
 
     public void run() {
+        double lastFrameTime = 0.0;
         while (true) {
-            // do nothing
+            double time = Time.getTime();
+            double deltaTime = time - lastFrameTime;
+            lastFrameTime = time;
+
+            update(deltaTime);
+
+            try{ Thread.sleep(30); } catch (Exception _) { }
         }
     }
 }
